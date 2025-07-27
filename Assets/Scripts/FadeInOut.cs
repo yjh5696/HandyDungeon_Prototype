1369c04c4987300d6a9dfd8940e00eb0c12346bb
Superdodge <yjh01209@gmail.com>
2025-07-27 20:06:24 +0900

    Added Scripts

--- Assets/Scripts/FadeInOut.cs
+++ Assets/Scripts/FadeInOut.cs
@@ -1,29 +1,73 @@
+using System;
+using System.Threading;
 using UnityEngine;
 using Cysharp.Threading.Tasks;
 
 public class FadeInOut : MonoBehaviour
 {
-    public bool isFadeIn;
+    public bool isFadeIn = true;
     
+    private CancellationTokenSource disableCancellation = new CancellationTokenSource();
+
+    private void OnEnable()
+    {
+        if (disableCancellation != null)
+        {
+            disableCancellation.Dispose();
+        }
+        disableCancellation = new CancellationTokenSource();
+    }
+
+    private void OnDisable()
+    {
+        disableCancellation.Cancel();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         if (isFadeIn)
         {
             gameObject.SetActive(true);
-            
+            UFadeOut().Forget();
         }
     }
 
-    private async UniTaskVoid FadeIn()
+    public void FadeIn()
+    {
+        gameObject.SetActive(true);
+        UFadeIn().Forget();
+    }
+
+    private async UniTaskVoid UFadeOut()
     {
         float elapsedTime = 0f;
         float fadeDuration = 0.5f;
 
         while (elapsedTime < fadeDuration)
         {
-            gameObject.GetComponent<CanvasRenderer>().SetAlpha(Mathf.Lerp(1f, 0f, elapsedTime / fadeDuration));
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, Mathf.Lerp(1f, 0f, elapsedTime / fadeDuration));
             elapsedTime += Time.deltaTime;
+            
+            await UniTask.Yield();
         }
+        
+        gameObject.SetActive(false);
+    }
+
+    private async UniTaskVoid UFadeIn()
+    {
+        float elapsedTime = 0f;
+        float fadeDuration = 0.5f;
+
+        while (elapsedTime < fadeDuration)
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, Mathf.Lerp(0f, 1f, elapsedTime / fadeDuration));
+            elapsedTime += Time.deltaTime;
+            
+            await UniTask.Yield();
+        }
+        
+        gameObject.SetActive(false);
     }
 }
