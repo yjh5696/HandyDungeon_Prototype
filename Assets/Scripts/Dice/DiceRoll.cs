9c7489f64ae1aa8e370df06aadaf11a6ce083bf0
ljh_6200 <86048372+ljh6200@users.noreply.github.com>
2025-08-31 03:21:46 +0900

    Update DiceRoll

--- Assets/Scripts/Dice/DiceRoll.cs
+++ Assets/Scripts/Dice/DiceRoll.cs
@@ -1,31 +1,36 @@
 using DG.Tweening;
 using UnityEngine;
-using UnityEngine.Serialization;
+using System;
+using Random = UnityEngine.Random;
 
 public class DiceRoll : MonoBehaviour
 {
-    public Sprite[] dices; // Inspector���� Dice_1~Dice_6 ������ ����
+    public Sprite[] dices;
     private SpriteRenderer _renderer;
     [SerializeField] private int DiceRollTime;
-    public int DiceResult { get; private set; } // 0~5 �� +1�ϸ� 1~6
+    public int DiceResult { get; private set; }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Sequence seq;
+    private bool isRolling = false;
+
     void Start()
     {
         _renderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RollDice(Action<int> onResult = null)
     {
-        
-    }
+        if (isRolling)
+        {
+            // 이미 굴림 중이면 무시 또는 조치 - 여기선 그냥 리턴
+            return;
+        }
 
-    public void RollDice(System.Action<int> onResult = null)
-    {
-        Sequence seq = DOTween.Sequence();
+        isRolling = true;
+        seq = DOTween.Sequence();
         int randomSprite = 0;
         int animationFrameCount = DiceRollTime * 20;
+
         for (int i = 0; i < animationFrameCount; i++)
         {
             seq.AppendCallback(() => {
@@ -34,11 +39,28 @@ public class DiceRoll : MonoBehaviour
             });
             seq.AppendInterval(0.05f);
         }
-
         seq.AppendCallback(() => {
             DiceResult = Random.Range(0, dices.Length);
             _renderer.sprite = dices[DiceResult];
-            onResult?.Invoke(DiceResult + 1); // 1~6 ����
+            onResult?.Invoke(DiceResult + 1);
+            isRolling = false;
+            seq = null;
         });
+        seq.Play();
+    }
+
+    // 주사위 애니메이션이 돌아가는 중 즉시 결과를 확정하는 함수
+    public void ForceFinishRoll(Action<int> onResult)
+    {
+        if (isRolling && seq != null)
+        {
+            seq.Kill();  // 애니메이션 강제 종료
+            DiceResult = Random.Range(0, dices.Length);
+            _renderer.sprite = dices[DiceResult];
+            onResult?.Invoke(DiceResult + 1);
+            isRolling = false;
+            seq = null;
+        }
     }
 }
+
