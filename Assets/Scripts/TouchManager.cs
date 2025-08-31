2e38283883242e07a4825c3a14c989f9b1105db7
Superdodge <yjh01209@gmail.com>
2025-08-31 22:31:28 +0900

    Add Touch Input / 로그 클릭 시 확장-축소 가능

---
+++ Assets/Scripts/TouchManager.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class TouchManager : MonoBehaviour
+{
+    public static TouchManager Instance;
+    
+    private TouchAction _touchAction;
+    [SerializeField] private GraphicRaycaster graphicRaycaster;
+    
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        
+        _touchAction = new TouchAction();
+    }
+    
+    private void OnEnable()
+    {
+        _touchAction.Enable();
+        _touchAction.Touch.Tap.performed += OnLogTapped;
+    }
+
+    private void OnLogTapped(InputAction.CallbackContext obj)
+    {
+        if (Camera.main == null)
+        {
+            Debug.LogWarning("No camera found");
+            return;
+        }
+
+        if (graphicRaycaster == null)
+        {
+            return;
+        }
+
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = _touchAction.Touch.Position.ReadValue<Vector2>();
+        List<RaycastResult> results = new List<RaycastResult>();
+        graphicRaycaster.Raycast(eventData, results);
+        foreach (var result in results.Where(result => result.gameObject.name == "Log"))
+        {
+            LogManager.Instance.ExpandLog();
+        }
+    }
+    
+    private void OnDisable()
+    {
+        _touchAction.Disable();
+        _touchAction.Touch.Tap.performed -= OnLogTapped;
+    }
+}
